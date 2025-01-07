b83f8b4f2820723e163bd1f825943f9a4c95a21b
Camille Darley <camille@pad.fr>
2025-01-08 00:37:22 +0100

    add vite config for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import path from "path";
 
 export default defineConfig({
     plugins: [react()],
+    build: {
+      outDir: path.join(__dirname, "_static")
+    }
 }); 
